9c52ce20cf90682a0f596b80849272e363ac15a6
Nic Jansma <nic@nicj.net>
2015-04-07 14:23:36 -0400

    Test lint fixes

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ mPulse.init("WWMEP-S65ZL-MMXA7-LRRWJ-9J9VA", {
 });
 
 for (var i = 0; i < 100; i++) {
-    mPulse.setDimension("Dimension 1", "farts" + Math.round(Math.random() * 10));
-    mPulse.setViewGroup("VG " +  + Math.round(Math.random() * 10))
-    mPulse.sendMetric("foo", Math.round(Math.random() * 10));
-    mPulse.sendTimer("Custom Timer 1", Math.round(Math.random() * 1000));
+    mPulse.setDimension("Dimension 1", "val" + Math.round(Math.random() * 10));
+    mPulse.setViewGroup("View Group " + Math.round(Math.random() * 10));
+    mPulse.sendMetric("Metric 1", Math.round(Math.random() * 10));
+    mPulse.sendTimer("Timer 1", Math.round(Math.random() * 1000));
 }
